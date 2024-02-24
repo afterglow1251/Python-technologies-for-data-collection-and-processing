84b43a36e4ca30d4b0b4f645842ca38a46208ade
afterglow1251 <157438603+afterglow1251@users.noreply.github.com>
2024-02-24 14:16:05 +0200

    Update Data input and output.py

--- Data input and output.py
+++ Data input and output.py
@@ -1,3 +1,4 @@
+#1
 name = input("What is your name? ")
 age = input("How old are you? ")
 location = input("Where are you live? ")
@@ -5,3 +6,20 @@ location = input("Where are you live? ")
 print("This is", name)
 print("It is", age)
 print("(S)he lives in", location)
+
+#2
+expression = "4 * 100 - 54"
+result = eval(expression)  # Використовуємо функцію eval() для обчислення виразу
+user_answer = int(input(f"Solve the expression {expression}: "))
+print("Correct answer:", result)
+print("Your answer:", user_answer)
+
+#3
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum1 = num1 + num2
+sum2 = float(input("Enter the third number: ")) + float(input("Enter the fourth number: "))
+
+result = round(sum1 / sum2, 2)
+print("Result:", result)
