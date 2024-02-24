3643101540ec1e94f8c61a521d4357b4dd712e33
afterglow1251 <157438603+afterglow1251@users.noreply.github.com>
2024-02-24 14:27:09 +0200

    Create Data input and output.py

---
+++ practice1/Data input and output.py
@@ -0,0 +1,25 @@
+#1
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you live? ")
+
+print("This is", name)
+print("It is", age)
+print("(S)he lives in", location)
+
+#2
+expression = "4 * 100 - 54"
+result = eval(expression)  
+user_answer = int(input(f"Solve the expression {expression}: "))
+print("Correct answer:", result)
+print("Your answer:", user_answer)
+
+#3
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum1 = num1 + num2
+sum2 = float(input("Enter the third number: ")) + float(input("Enter the fourth number: "))
+
+result = round(sum1 / sum2, 2)
+print("Result:", result)
